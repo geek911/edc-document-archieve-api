9761fd5506193fc11d6b6e6c682f5cc1bc6ce8bf
MoffatMore <mofenyimoffat@gmail.com>
2022-03-09 10:19:21 +0200

    exclude other files

--- build/lib/edc_document_archieve/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig as DjangoAppConfig
-
-
-class AppConfig(DjangoAppConfig):
-    name = 'edc_document_archieve'
-    verbose_name = "EDC Document Archieve"
-    admin_site_name = 'edc_document_archieve_admin'
